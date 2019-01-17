a7c5f8b81acaeb23633f9b62e5c0a6833519a67f
WilliamDH <43973219+WilliamDH@users.noreply.github.com>
2019-01-17 14:26:58 -0600

    created routes folder and added apiRoutes.js and htmlRoutes.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Run API routes before routing to React
+require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -14,6 +24,22 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+var syncOptions = { force: false };
+
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+};
+
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
+
+module.exports = app;
